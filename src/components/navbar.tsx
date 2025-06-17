0e216c9964eeaa94d1a1d4b770f420d318f174ff
aXLan666 <angaragilang266@gmail.com>
2025-06-17 18:51:23 +0700

    feat(components): add Portfolio link with icon to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -48,6 +48,28 @@ function Navbar() {
               <li>
                 <Link href="/">Cryptocurrency</Link>
               </li>
+              <li>
+                <Link href="/" className="flex items-center gap-0.5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2.25 13.5a8.25 8.25 0 0 1 8.25-8.25.75.75 0 0 1 .75.75v6.75H18a.75.75 0 0 1 .75.75 8.25 8.25 0 0 1-16.5 0Z"
+                      clipRule="evenodd"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M12.75 3a.75.75 0 0 1 .75-.75 8.25 8.25 0 0 1 8.25 8.25.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75V3Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  Portfolio
+                </Link>
+              </li>
             </ul>
           </div>
           <NavbarToggle />
@@ -107,6 +129,30 @@ function Navbar() {
           <NavbarCollapse>
             <NavbarLink as={Link} href="/">
               Cryptocurrency
+            </NavbarLink>
+            <NavbarLink
+              as={Link}
+              href="/portfolio"
+              className="flex items-center gap-0.5"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 13.5a8.25 8.25 0 0 1 8.25-8.25.75.75 0 0 1 .75.75v6.75H18a.75.75 0 0 1 .75.75 8.25 8.25 0 0 1-16.5 0Z"
+                  clipRule="evenodd"
+                />
+                <path
+                  fillRule="evenodd"
+                  d="M12.75 3a.75.75 0 0 1 .75-.75 8.25 8.25 0 0 1 8.25 8.25.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75V3Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Portfolio
             </NavbarLink>
             <Button
               size="sm"
